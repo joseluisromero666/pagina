b6124d2c806d4aeb15a5eed1a24a64834fdd43bf
joseluisromero666 <josel.romerom@konradlorenz.edu.co>
2020-04-23 16:09:57 -0500

    version 0.1

---
+++ funciones.js
@@ -0,0 +1,17 @@
+function RegresarIN(){
+  window.open("index.html","_self");
+}
+  
+  function RegresarSe(){
+    window.open("Series.html","_self");
+}
+function RegresarEn(){
+  window.open("Emision.html","_self");
+}
+
+function RegresarSuscrp(){
+  window.open("suscripcion.html","_self");
+}
+function RegresarOk(){
+  window.open("ok.html","_self");
+}
