7f5bb866fd58b365f7fce352c4eeacf61839672d
joseluisromero666 <josel.romerom@konradlorenz.edu.co>
2020-05-20 20:24:11 -0500

    version 0.1

--- funciones.js
+++ funciones.js
@@ -39,13 +39,23 @@ function validarForm() {
         return false;
     }
     if ($("#email").val() == "") {
-        alert("El campo 'mail' no puede estar vacio.");
+        alert("El campo 'email' no puede estar vacio.");
         $("#email").focus();
         return false;
     }
     if ($("#pass").val() == "") {
         alert("El campo 'Contraseña' no puede estar vacio.");
         $("#pass").focus();
+        return false;
+    }
+    if ($("#Compañía").val() == "") {
+        alert("El campo 'Compañía' no puede estar vacio.");
+        $("#Compañía").focus();
+        return false;
+    }
+    if ($("#Teléfono").val() == "") {
+        alert("El campo 'Teléfono' no puede estar vacio.");
+        $("#Teléfono").focus();
         return false;
     }
 
@@ -66,6 +76,30 @@ $(document).ready(function() {
             $("#fracaso").delay(500).fadeln("slow");
             $("#fracaso").delay(5000).fadeOut("slow");
         }
+    });
+});
+
+$(document).ready(function() {
+    $("#botonenviar2").click(function() {
+        if (validarForm2()) {
+            if ($("#fracaso").is(":visible")) {
+                $("#fracaso").fadeOut();
+            }
+            $("#exito").delay(500).fadeln("slow");
+        } else {
+            if ($("#exito").is(":visible")) {
+                $("#exito").fadeOut("fast");
+            }
+            $("#fracaso").delay(500).fadeln("slow");
+            $("#fracaso").delay(5000).fadeOut("slow");
+        }
+    });
+});
+$(document).ready(function() {
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
     });
 });
 
@@ -99,4 +133,100 @@ function Gekokujou() {
 
 function Tamayomi() {
     window.open("Tamayomi.html", "_self");
+}
+
+function Diamond() {
+    window.open("Diamond.html", "_self");
+}
+
+function Phantasy() {
+    window.open("Phantasy.html", "_self");
+}
+
+function Backstage() {
+    window.open("Backstage.html", "_self");
+}
+
+function Pet() {
+    window.open("Pet.html", "_self");
+}
+
+function Gegege() {
+    window.open("Gegege.html", "_self");
+}
+
+function Boruto() {
+    window.open("Boruto.html", "_self");
+}
+
+function One_Piece() {
+    window.open("One_Piece.html", "_self");
+}
+
+function Ishuzoku() {
+    window.open("Ishuzoku.html", "_self");
+}
+
+function Kyokou() {
+    window.open("Kyokou.html", "_self");
+}
+
+function Black_Clover() {
+    window.open("Black_Clover.html", "_self");
+}
+
+function Re_Zero() {
+    window.open("Re_Zero.html", "_self");
+}
+
+function Hachi_nan() {
+    window.open("Hachi_nan.html", "_self");
+}
+
+function Plunderer() {
+    window.open("Plunderer.html", "_self");
+}
+
+function Infinite() {
+    window.open("Infinite.html", "_self");
+}
+
+function Kaguya_sama() {
+    window.open("Kaguya_sama.html", "_self");
+}
+
+function Shironeko() {
+    window.open("Shironeko.html", "_self");
+}
+
+function Isekai() {
+    window.open("Isekai.html", "_self");
+}
+
+function Ahiru() {
+    window.open("Ahiru.html", "_self");
+}
+
+function Gleipnir() {
+    window.open("Gleipnir.html", "_self");
+}
+
+function Dorohedoro() {
+    window.open("Dorohedoro.html", "_self");
+}
+
+function validarForm2() {
+
+    if ($("#email2").val() == "") {
+        alert("El campo 'email' no puede estar vacio.");
+        $("#email2").focus();
+        return false;
+    }
+    if ($("#pass2").val() == "") {
+        alert("El campo 'Contraseña' no puede estar vacio.");
+        $("#pass2").focus();
+        return false;
+    }
+    window.open("index.html", "_self");
+    return true;
 }
